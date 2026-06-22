dc927d6aa501badaba2613ad2afb2c2be7d2f2ce
lowkkid <shikunov.dev@gmail.com>
2026-06-22 17:30:52 +0300

    finished challenge

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('Проект запущен');
+const button = document.getElementById("share-btn");
+const share = document.getElementById("share");
+
+let isOpen = false;
+
+document.addEventListener("click", function (e) {
+    if (isOpen && !share.contains(e.target) && !button.contains(e.target)) {
+        toggleShare();
+    }
+})
+
+button.addEventListener("click", function () {
+    toggleShare();
+
+})
+
+function toggleShare() {
+    isOpen = !isOpen;
+    button.classList.toggle("hover:bg-gray-300");
+    if (button.classList.toggle("bg-gray-200")) {
+        button.children[0].children[0].setAttribute("fill", "#6E8098");
+    }
+
+    button.classList.toggle("hover:bg-gray-400");
+    if (button.classList.toggle("bg-gray-500")) {
+        button.children[0].children[0].setAttribute("fill", "#FFFFFF");
+    }
+
+
+    share.classList.toggle("hidden");
+    share.classList.toggle("flex");
+}
